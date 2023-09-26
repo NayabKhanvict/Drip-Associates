9b8ce62dac65ab1a1a5391b9314deedc00a85b27
NayabKhanvict <nayabkhanvict@gmail.com>
2023-09-26 19:04:51 +0500

    Provider component conflict resolve

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -1,27 +1,4 @@
 "use client";
-<<<<<<< HEAD
-// "use client";
-// import { Provider } from "react-redux";
-// import { SessionProvider } from "next-auth/react";
-// import { persistor, store } from "@/redux/store";
-// import { PersistGate } from "redux-persist/integration/react";
-
-// const Layout = ({ children }: { children: React.ReactNode }) => {
-//   return (
-//     <Provider store={store}>
-//       <PersistGate persistor={persistor}>
-//         <SessionProvider>{children}</SessionProvider>
-//       </PersistGate>
-//     </Provider>
-//   );
-// };
-
-// export default Layout;
-
-import "../styles/bootstrap.min.css";
-import "../styles/animate.min.css";
-import "../styles/fontawesome.min.css";
-=======
 import "../styles/bootstrap.min.css";
 import "../styles/animate.min.css";
 import "../styles/fontawesome.min.css";
@@ -29,7 +6,6 @@ import "react-accessible-accordion/dist/fancy-example.css";
 import "swiper/css";
 import "swiper/css/bundle";
 
->>>>>>> develop
 // Globals CSS
 import "../styles/style.css";
 // Responsive CSS
